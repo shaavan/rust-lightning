e1133414d01477e2eb65b3f0b8eaf160f64c619b
shaavan <shaavan.github@gmail.com>
2024-05-09 17:19:07 +0530

    Introduce RecipientData in ReceiveTlvs
    1. This struct will be used by the node to add data along with the
       reply_path sent to the counterparty. The node expects to
       receive this data back and use it for doing further processing.
    2. In its first iteration, RecipientData contains the payment_id of
       the corresponding Outbound BOLT12 Payment.
    3. This will be used in a later commit to abandon outbound payments
       that have failed to complete for some reason.
    
    Update:
    1. Modify the RecipientData into enum. That will allow us to seperate
       Offers RecipientData from Custom RecipientData.
    2. Assumption: If no recipient data is provided create a
       RecipientData::Custom variant.

--- lightning/src/onion_message/packet.rs
+++ lightning/src/onion_message/packet.rs
@@ -13,7 +13,7 @@ use bitcoin::secp256k1::PublicKey;
 use bitcoin::secp256k1::ecdh::SharedSecret;
 
 use crate::blinded_path::{BlindedPath, NextMessageHop};
-use crate::blinded_path::message::{ForwardTlvs, ReceiveTlvs};
+use crate::blinded_path::message::{ForwardTlvs, OffersData, ReceiveTlvs, RecipientData};
 use crate::blinded_path::utils::Padding;
 use crate::ln::msgs::DecodeError;
 use crate::ln::onion_utils;
@@ -304,6 +304,8 @@ impl Readable for ControlTlvs {
 			(4, next_node_id, option),
 			(6, path_id, option),
 			(8, next_blinding_override, option),
+			(65537, payment_id, option),
+			(65539, custom_data, optional_vec),
 		});
 		let _padding: Option<Padding> = _padding;
 
@@ -317,6 +319,22 @@ impl Readable for ControlTlvs {
 		let valid_fwd_fmt = next_hop.is_some() && path_id.is_none();
 		let valid_recv_fmt = next_hop.is_none() && next_blinding_override.is_none();
 
+		// Make sure not both offer recipient_data (aka payment_id), and custom recipient_data, are provided together.
+		let data = match custom_data {
+			Some(data) => data,
+			None => {
+				assert!(false);
+				Vec::new()
+			},
+		};
+		assert!(payment_id.is_none() || data.is_empty());
+		
+		let recipient_data = if let Some(payment_id) = payment_id {
+			RecipientData::Offers(OffersData::new(payment_id))
+		} else {
+			RecipientData::Custom(data)
+		};
+
 		let payload_fmt = if valid_fwd_fmt {
 			ControlTlvs::Forward(ForwardTlvs {
 				next_hop: next_hop.unwrap(),
@@ -325,6 +343,7 @@ impl Readable for ControlTlvs {
 		} else if valid_recv_fmt {
 			ControlTlvs::Receive(ReceiveTlvs {
 				path_id,
+				recipient_data,
 			})
 		} else {
 			return Err(DecodeError::InvalidValue)
