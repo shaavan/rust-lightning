2debe05b37202ab0a21a28b4b3e9266d4687ffae
shaavan <shaavan.github@gmail.com>
2024-06-09 18:17:38 +0530

    Incomplete test

--- lightning/src/ln/offers_tests.rs
+++ lightning/src/ln/offers_tests.rs
@@ -542,10 +542,10 @@ fn creates_and_pays_for_refund_using_two_hop_blinded_path() {
 /// introduction node of the blinded path.
 #[test]
 fn creates_and_pays_for_offer_using_one_hop_blinded_path() {
-	let chanmon_cfgs = create_chanmon_cfgs(2);
-	let node_cfgs = create_node_cfgs(2, &chanmon_cfgs);
-	let node_chanmgrs = create_node_chanmgrs(2, &node_cfgs, &[None, None]);
-	let nodes = create_network(2, &node_cfgs, &node_chanmgrs);
+	let chanmon_cfgs = create_chanmon_cfgs(3);
+	let node_cfgs = create_node_cfgs(3, &chanmon_cfgs);
+	let node_chanmgrs = create_node_chanmgrs(3, &node_cfgs, &[None, None, None]);
+	let nodes = create_network(3, &node_cfgs, &node_chanmgrs);
 
 	create_announced_chan_between_nodes_with_value(&nodes, 0, 1, 10_000_000, 1_000_000_000);
 
@@ -553,6 +553,7 @@ fn creates_and_pays_for_offer_using_one_hop_blinded_path() {
 	let alice_id = alice.node.get_our_node_id();
 	let bob = &nodes[1];
 	let bob_id = bob.node.get_our_node_id();
+	let charlie = &nodes[2];
 
 	let offer = alice.node
 		.create_offer_builder().unwrap()
@@ -570,7 +571,12 @@ fn creates_and_pays_for_offer_using_one_hop_blinded_path() {
 	bob.node.pay_for_offer(&offer, None, None, None, payment_id, Retry::Attempts(0), None).unwrap();
 	expect_recent_payment!(bob, RecentPaymentDetails::AwaitingInvoice, payment_id);
 
+	let _ = bob.onion_messenger.next_onion_message_for_peer(alice_id).unwrap();
+
+	disconnect_peers(bob, &[charlie]);
+
 	let onion_message = bob.onion_messenger.next_onion_message_for_peer(alice_id).unwrap();
+
 	alice.onion_messenger.handle_onion_message(&bob_id, &onion_message);
 
 	let (invoice_request, reply_path) = extract_invoice_request(alice, &onion_message);
@@ -590,6 +596,16 @@ fn creates_and_pays_for_offer_using_one_hop_blinded_path() {
 
 	let onion_message = alice.onion_messenger.next_onion_message_for_peer(bob_id).unwrap();
 	bob.onion_messenger.handle_onion_message(&alice_id, &onion_message);
+
+
+	// We expect no more OffersMessage to be enqueued by this point, after the retry tick has occurred.
+	connect_peers(bob, charlie);
+
+	let result = bob.onion_messenger.next_onion_message_for_peer(alice_id);
+	match result {
+		Some(_) => panic!("Unexpected message enqueued after retry tick."),
+		None => assert!(true),
+	}
 
 	let invoice = extract_invoice(bob, &onion_message);
 	assert_eq!(invoice.amount_msats(), 10_000_000);
