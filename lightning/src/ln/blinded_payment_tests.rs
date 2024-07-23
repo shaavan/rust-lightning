e3885f03cda2add7aafa746a77dbed21823f9a3b
shaavan <shaavan.github@gmail.com>
2024-07-23 17:41:45 +0530

    Introduce Option<Padding> for ControlTlvs variants.
    - Introduce padding as a field in the `ForwardTlvs` and `ReceiveTlvs`
      structs to ensure the above condition is met.
    - Calculate the padding length prior to padding the TLVs and use that
      data to update the padding field later.
    
    Co-authored-by: Jeffrey Czyz <jkczyz@gmail.com>

--- lightning/src/ln/blinded_payment_tests.rs
+++ lightning/src/ln/blinded_payment_tests.rs
@@ -289,7 +289,7 @@ fn do_forward_checks_failure(check: ForwardCheckFail, intro_fails: bool) {
 	let mut route_params = get_blinded_route_parameters(amt_msat, payment_secret, 1, 1_0000_0000,
 		nodes.iter().skip(1).map(|n| n.node.get_our_node_id()).collect(),
 		&[&chan_upd_1_2, &chan_upd_2_3], &chanmon_cfgs[3].keys_manager);
-	route_params.payment_params.max_path_length = 18;
+	route_params.payment_params.max_path_length = 17;
 
 	let route = get_route(&nodes[0], &route_params).unwrap();
 	node_cfgs[0].router.expect_find_route(route_params.clone(), Ok(route.clone()));
