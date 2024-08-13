eddcc31d5ae1fef99fc9463ad7ebff5e00f8bdfb
shaavan <shaavan.github@gmail.com>
2024-08-13 15:03:56 +0530

    Remove `create_compact_blinded_paths`
    1. The `params` parameter now allows specifying the type of blinded
       path to be created, enabling a single function to handle the processing
       for both compact and full-length blinded paths.
    2. This change renders the `create_compact_blinded_paths` function redundant,
       leading to its removal.
    3. With this commit, the blinded path creation process is streamlined to
       a single function capable of creating any type of blinded path.

--- lightning/src/routing/router.rs
+++ lightning/src/routing/router.rs
@@ -197,17 +197,9 @@ impl< G: Deref<Target = NetworkGraph<L>>, L: Deref, ES: Deref, S: Deref, SP: Siz
 	fn create_blinded_paths<
 		T: secp256k1::Signing + secp256k1::Verification
 	> (
-		&self, params: BlindedPathParams, recipient: PublicKey, context: MessageContext, peers: Vec<PublicKey>, secp_ctx: &Secp256k1<T>,
+		&self, params: BlindedPathParams, recipient: PublicKey, context: MessageContext, peers: Vec<MessageForwardNode>, secp_ctx: &Secp256k1<T>,
 	) -> Result<Vec<BlindedMessagePath>, ()> {
 		DefaultMessageRouter::create_blinded_paths(params, &self.network_graph, recipient, context, peers, &self.entropy_source, secp_ctx)
-	}
-
-	fn create_compact_blinded_paths<
-		T: secp256k1::Signing + secp256k1::Verification
-	> (
-		&self, recipient: PublicKey, context: MessageContext, peers: Vec<MessageForwardNode>, secp_ctx: &Secp256k1<T>,
-	) -> Result<Vec<BlindedMessagePath>, ()> {
-		DefaultMessageRouter::create_compact_blinded_paths(&self.network_graph, recipient, context, peers, &self.entropy_source, secp_ctx)
 	}
 }
 
