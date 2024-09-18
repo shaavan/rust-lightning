8d0ba4a6fdfcdb3f98dd98dd8499c15ee98f9a6f
shaavan <shaavan.github@gmail.com>
2024-09-18 17:45:08 +0530

    Allow explicit specification of Blinded Path type in `create_offer_builder`
    1. This commit introduces flexibility for the user to specify the
       type of Blinded Path they want, rather than relying on the Offers'
       absolute expiry to determine the Blinded Path type.
    2. It also adds support for creating an Offer without a Blinded Path,
       using its signing public key for responding to the offer instead.

--- lightning/src/ln/offers_tests.rs
+++ lightning/src/ln/offers_tests.rs
@@ -59,7 +59,7 @@ use crate::offers::invoice_error::InvoiceError;
 use crate::offers::invoice_request::{InvoiceRequest, InvoiceRequestFields};
 use crate::offers::nonce::Nonce;
 use crate::offers::parse::Bolt12SemanticError;
-use crate::onion_message::messenger::{Destination, PeeledOnion, MessageSendInstructions};
+use crate::onion_message::messenger::{BlindedPathType, Destination, MessageSendInstructions, PeeledOnion};
 use crate::onion_message::offers::OffersMessage;
 use crate::onion_message::packet::ParsedOnionMessageContents;
 use crate::routing::gossip::{NodeAlias, NodeId};
@@ -301,7 +301,7 @@ fn prefers_non_tor_nodes_in_blinded_paths() {
 	announce_node_address(charlie, &[alice, bob, david, &nodes[4], &nodes[5]], tor.clone());
 
 	let offer = bob.node
-		.create_offer_builder(None).unwrap()
+		.create_offer_builder(Some(BlindedPathType::Full)).unwrap()
 		.amount_msats(10_000_000)
 		.build().unwrap();
 	assert_ne!(offer.signing_pubkey(), Some(bob_id));
@@ -317,7 +317,7 @@ fn prefers_non_tor_nodes_in_blinded_paths() {
 	announce_node_address(&nodes[5], &[alice, bob, charlie, david, &nodes[4]], tor.clone());
 
 	let offer = bob.node
-		.create_offer_builder(None).unwrap()
+		.create_offer_builder(Some(BlindedPathType::Full)).unwrap()
 		.amount_msats(10_000_000)
 		.build().unwrap();
 	assert_ne!(offer.signing_pubkey(), Some(bob_id));
@@ -368,7 +368,7 @@ fn prefers_more_connected_nodes_in_blinded_paths() {
 	disconnect_peers(david, &[bob, &nodes[4], &nodes[5]]);
 
 	let offer = bob.node
-		.create_offer_builder(None).unwrap()
+		.create_offer_builder(Some(BlindedPathType::Full)).unwrap()
 		.amount_msats(10_000_000)
 		.build().unwrap();
 	assert_ne!(offer.signing_pubkey(), Some(bob_id));
@@ -395,7 +395,8 @@ fn creates_short_lived_offer() {
 
 	let absolute_expiry = alice.node.duration_since_epoch() + MAX_SHORT_LIVED_RELATIVE_EXPIRY;
 	let offer = alice.node
-		.create_offer_builder(Some(absolute_expiry)).unwrap()
+		.create_offer_builder(Some(BlindedPathType::Compact)).unwrap()
+		.absolute_expiry(absolute_expiry)
 		.build().unwrap();
 	assert_eq!(offer.absolute_expiry(), Some(absolute_expiry));
 	assert!(!offer.paths().is_empty());
@@ -422,8 +423,8 @@ fn creates_long_lived_offer() {
 	let absolute_expiry = alice.node.duration_since_epoch() + MAX_SHORT_LIVED_RELATIVE_EXPIRY
 		+ Duration::from_secs(1);
 	let offer = alice.node
-		.create_offer_builder(Some(absolute_expiry))
-		.unwrap()
+		.create_offer_builder(Some(BlindedPathType::Full)).unwrap()
+		.absolute_expiry(absolute_expiry)
 		.build().unwrap();
 	assert_eq!(offer.absolute_expiry(), Some(absolute_expiry));
 	assert!(!offer.paths().is_empty());
@@ -432,7 +433,7 @@ fn creates_long_lived_offer() {
 	}
 
 	let offer = alice.node
-		.create_offer_builder(None).unwrap()
+		.create_offer_builder(Some(BlindedPathType::Full)).unwrap()
 		.build().unwrap();
 	assert_eq!(offer.absolute_expiry(), None);
 	assert!(!offer.paths().is_empty());
@@ -536,7 +537,7 @@ fn creates_and_pays_for_offer_using_two_hop_blinded_path() {
 	disconnect_peers(david, &[bob, &nodes[4], &nodes[5]]);
 
 	let offer = alice.node
-		.create_offer_builder(None)
+		.create_offer_builder(Some(BlindedPathType::Full))
 		.unwrap()
 		.amount_msats(10_000_000)
 		.build().unwrap();
@@ -694,7 +695,7 @@ fn creates_and_pays_for_offer_using_one_hop_blinded_path() {
 	let bob_id = bob.node.get_our_node_id();
 
 	let offer = alice.node
-		.create_offer_builder(None).unwrap()
+		.create_offer_builder(Some(BlindedPathType::Full)).unwrap()
 		.amount_msats(10_000_000)
 		.build().unwrap();
 	assert_ne!(offer.signing_pubkey(), Some(alice_id));
@@ -814,7 +815,7 @@ fn pays_for_offer_without_blinded_paths() {
 	let bob_id = bob.node.get_our_node_id();
 
 	let offer = alice.node
-		.create_offer_builder(None).unwrap()
+		.create_offer_builder(Some(BlindedPathType::Full)).unwrap()
 		.clear_paths()
 		.amount_msats(10_000_000)
 		.build().unwrap();
@@ -937,7 +938,7 @@ fn send_invoice_requests_with_distinct_reply_path() {
 	disconnect_peers(david, &[bob, &nodes[4], &nodes[5]]);
 
 	let offer = alice.node
-		.create_offer_builder(None)
+		.create_offer_builder(Some(BlindedPathType::Full))
 		.unwrap()
 		.amount_msats(10_000_000)
 		.build().unwrap();
@@ -1075,7 +1076,7 @@ fn pays_bolt12_invoice_asynchronously() {
 	let bob_id = bob.node.get_our_node_id();
 
 	let offer = alice.node
-		.create_offer_builder(None).unwrap()
+		.create_offer_builder(Some(BlindedPathType::Full)).unwrap()
 		.amount_msats(10_000_000)
 		.build().unwrap();
 
@@ -1159,7 +1160,7 @@ fn creates_offer_with_blinded_path_using_unannounced_introduction_node() {
 	let bob_id = bob.node.get_our_node_id();
 
 	let offer = alice.node
-		.create_offer_builder(None).unwrap()
+		.create_offer_builder(Some(BlindedPathType::Full)).unwrap()
 		.amount_msats(10_000_000)
 		.build().unwrap();
 	assert_ne!(offer.signing_pubkey(), Some(alice_id));
@@ -1287,7 +1288,7 @@ fn fails_authentication_when_handling_invoice_request() {
 	disconnect_peers(david, &[bob, &nodes[4], &nodes[5]]);
 
 	let offer = alice.node
-		.create_offer_builder(None)
+		.create_offer_builder(Some(BlindedPathType::Full))
 		.unwrap()
 		.amount_msats(10_000_000)
 		.build().unwrap();
@@ -1299,7 +1300,7 @@ fn fails_authentication_when_handling_invoice_request() {
 	}
 
 	let invalid_path = alice.node
-		.create_offer_builder(None)
+		.create_offer_builder(Some(BlindedPathType::Full))
 		.unwrap()
 		.build().unwrap()
 		.paths().first().unwrap()
@@ -1399,7 +1400,7 @@ fn fails_authentication_when_handling_invoice_for_offer() {
 	disconnect_peers(david, &[bob, &nodes[4], &nodes[5]]);
 
 	let offer = alice.node
-		.create_offer_builder(None)
+		.create_offer_builder(Some(BlindedPathType::Full))
 		.unwrap()
 		.amount_msats(10_000_000)
 		.build().unwrap();
@@ -1596,7 +1597,7 @@ fn fails_creating_or_paying_for_offer_without_connected_peers() {
 	disconnect_peers(david, &[bob, charlie, &nodes[4], &nodes[5]]);
 
 	let absolute_expiry = alice.node.duration_since_epoch() + MAX_SHORT_LIVED_RELATIVE_EXPIRY;
-	match alice.node.create_offer_builder(Some(absolute_expiry)) {
+	match alice.node.create_offer_builder(Some(BlindedPathType::Full)) {
 		Ok(_) => panic!("Expected error"),
 		Err(e) => assert_eq!(e, Bolt12SemanticError::MissingPaths),
 	}
@@ -1606,7 +1607,8 @@ fn fails_creating_or_paying_for_offer_without_connected_peers() {
 	reconnect_nodes(args);
 
 	let offer = alice.node
-		.create_offer_builder(Some(absolute_expiry)).unwrap()
+		.create_offer_builder(Some(BlindedPathType::Full)).unwrap()
+		.absolute_expiry(absolute_expiry)
 		.amount_msats(10_000_000)
 		.build().unwrap();
 
@@ -1710,7 +1712,7 @@ fn fails_creating_invoice_request_for_unsupported_chain() {
 	let bob = &nodes[1];
 
 	let offer = alice.node
-		.create_offer_builder(None).unwrap()
+		.create_offer_builder(Some(BlindedPathType::Full)).unwrap()
 		.clear_chains()
 		.chain(Network::Signet)
 		.build().unwrap();
@@ -1769,7 +1771,7 @@ fn fails_creating_invoice_request_without_blinded_reply_path() {
 	disconnect_peers(david, &[bob, charlie, &nodes[4], &nodes[5]]);
 
 	let offer = alice.node
-		.create_offer_builder(None).unwrap()
+		.create_offer_builder(Some(BlindedPathType::Full)).unwrap()
 		.amount_msats(10_000_000)
 		.build().unwrap();
 
@@ -1803,7 +1805,7 @@ fn fails_creating_invoice_request_with_duplicate_payment_id() {
 	disconnect_peers(alice, &[charlie, david, &nodes[4], &nodes[5]]);
 
 	let offer = alice.node
-		.create_offer_builder(None).unwrap()
+		.create_offer_builder(Some(BlindedPathType::Full)).unwrap()
 		.amount_msats(10_000_000)
 		.build().unwrap();
 
@@ -1889,7 +1891,7 @@ fn fails_sending_invoice_without_blinded_payment_paths_for_offer() {
 	disconnect_peers(david, &[bob, &nodes[4], &nodes[5]]);
 
 	let offer = alice.node
-		.create_offer_builder(None).unwrap()
+		.create_offer_builder(Some(BlindedPathType::Full)).unwrap()
 		.amount_msats(10_000_000)
 		.build().unwrap();
 
@@ -2098,7 +2100,7 @@ fn fails_paying_invoice_with_unknown_required_features() {
 	disconnect_peers(david, &[bob, &nodes[4], &nodes[5]]);
 
 	let offer = alice.node
-		.create_offer_builder(None).unwrap()
+		.create_offer_builder(Some(BlindedPathType::Full)).unwrap()
 		.amount_msats(10_000_000)
 		.build().unwrap();
 
