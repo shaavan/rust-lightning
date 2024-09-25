ab0cd6bdbde32ba82aff58d96b0044cd0c433d22
shaavan <shaavan.github@gmail.com>
2024-09-25 14:56:46 +0530

    Extend pay_for_offer to accept ManualRoutingParameters
    This update allows users to call `pay_for_offer` with a set of parameters
    they wish to manually set for routing the corresponding invoice. By
    accepting `ManualRoutingParameters`, users gain greater control over the
    routing process.

--- lightning/src/ln/outbound_payment.rs
+++ lightning/src/ln/outbound_payment.rs
@@ -1598,13 +1598,9 @@ impl OutboundPayments {
 
 	pub(super) fn add_new_awaiting_invoice(
 		&self, payment_id: PaymentId, expiration: StaleExpiration, retry_strategy: Retry,
-		max_total_routing_fee_msat: Option<u64>, retryable_invoice_request: Option<RetryableInvoiceRequest>
+		manual_routing_params: Option<ManualRoutingParameters>, retryable_invoice_request: Option<RetryableInvoiceRequest>
 	) -> Result<(), ()> {
 		let mut pending_outbounds = self.pending_outbound_payments.lock().unwrap();
-		let manual_routing_params = max_total_routing_fee_msat.map(
-			|fee_msats| ManualRoutingParameters::new()
-				.with_max_total_routing_fee_msat(fee_msats)
-		);
 		match pending_outbounds.entry(payment_id) {
 			hash_map::Entry::Occupied(_) => Err(()),
 			hash_map::Entry::Vacant(entry) => {
@@ -2281,7 +2277,7 @@ mod tests {
 	use crate::offers::offer::OfferBuilder;
 	use crate::offers::test_utils::*;
 	use crate::routing::gossip::NetworkGraph;
-	use crate::routing::router::{InFlightHtlcs, Path, PaymentParameters, Route, RouteHop, RouteParameters};
+	use crate::routing::router::{InFlightHtlcs, ManualRoutingParameters, Path, PaymentParameters, Route, RouteHop, RouteParameters};
 	use crate::sync::{Arc, Mutex, RwLock};
 	use crate::util::errors::APIError;
 	use crate::util::hash_tables::new_hash_map;
@@ -2695,10 +2691,12 @@ mod tests {
 			.build().unwrap()
 			.sign(recipient_sign).unwrap();
 
+		let manual_routing_params = ManualRoutingParameters::new().with_max_total_routing_fee_msat(invoice.amount_msats() / 100 + 50_000);
+
 		assert!(
 			outbound_payments.add_new_awaiting_invoice(
 				payment_id, expiration, Retry::Attempts(0),
-				Some(invoice.amount_msats() / 100 + 50_000), None,
+				Some(manual_routing_params), None,
 			).is_ok()
 		);
 		assert!(outbound_payments.has_pending_payments());
@@ -2796,9 +2794,11 @@ mod tests {
 		assert!(!outbound_payments.has_pending_payments());
 		assert!(pending_events.lock().unwrap().is_empty());
 
+		let manual_routing_params = ManualRoutingParameters::new().with_max_total_routing_fee_msat(1234);
+
 		assert!(
 			outbound_payments.add_new_awaiting_invoice(
-				payment_id, expiration, Retry::Attempts(0), Some(1234), None,
+				payment_id, expiration, Retry::Attempts(0), Some(manual_routing_params), None,
 			).is_ok()
 		);
 		assert!(outbound_payments.has_pending_payments());
