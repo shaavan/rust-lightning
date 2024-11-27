f6d4f5832f2f94d0d9452f0e1cfecaa0e74aa195
shaavan <shaavan.github@gmail.com>
2024-11-27 18:06:27 +0530

    Move create_blinded_path_using_absolute_expiry to flow.rs

--- lightning/src/onion_message/messenger.rs
+++ lightning/src/onion_message/messenger.rs
@@ -2106,9 +2106,23 @@ pub type SimpleArcOnionMessenger<M, T, F, L> = OnionMessenger<
 	Arc<L>,
 	Arc<SimpleArcChannelManager<M, T, F, L>>,
 	Arc<DefaultMessageRouter<Arc<NetworkGraph<Arc<L>>>, Arc<L>, Arc<KeysManager>>>,
-	Arc<OffersMessageFlow<Arc<KeysManager>, Arc<SimpleArcChannelManager<M, T, F, L>>, Arc<L>>>,
+	Arc<
+		OffersMessageFlow<
+			Arc<KeysManager>,
+			Arc<SimpleArcChannelManager<M, T, F, L>>,
+			Arc<DefaultMessageRouter<Arc<NetworkGraph<Arc<L>>>, Arc<L>, Arc<KeysManager>>>,
+			Arc<L>,
+		>,
+	>,
 	Arc<SimpleArcChannelManager<M, T, F, L>>,
-	Arc<OffersMessageFlow<Arc<KeysManager>, Arc<SimpleArcChannelManager<M, T, F, L>>, Arc<L>>>,
+	Arc<
+		OffersMessageFlow<
+			Arc<KeysManager>,
+			Arc<SimpleArcChannelManager<M, T, F, L>>,
+			Arc<DefaultMessageRouter<Arc<NetworkGraph<Arc<L>>>, Arc<L>, Arc<KeysManager>>>,
+			Arc<L>,
+		>,
+	>,
 	IgnoringMessageHandler,
 >;
 
@@ -2127,7 +2141,14 @@ pub type SimpleArcOnionMessenger<M, T, F, L> = OnionMessenger<
 	Arc<L>,
 	Arc<SimpleArcChannelManager<M, T, F, L>>,
 	Arc<DefaultMessageRouter<Arc<NetworkGraph<Arc<L>>>, Arc<L>, Arc<KeysManager>>>,
-	Arc<OffersMessageFlow<Arc<KeysManager>, Arc<SimpleArcChannelManager<M, T, F, L>>, Arc<L>>>,
+	Arc<
+		OffersMessageFlow<
+			Arc<KeysManager>,
+			Arc<SimpleArcChannelManager<M, T, F, L>>,
+			Arc<DefaultMessageRouter<Arc<NetworkGraph<Arc<L>>>, Arc<L>, Arc<KeysManager>>>,
+			Arc<L>,
+		>,
+	>,
 	Arc<SimpleArcChannelManager<M, T, F, L>>,
 	IgnoringMessageHandler,
 	IgnoringMessageHandler,
