886424c5035dd0d4b9908afbb3ac4b939cbbd8c3
shaavan <shaavan.github@gmail.com>
2025-03-04 02:31:08 +0530

    Introduce Flow parameter in ChannelManager
    - Docs test failing

--- lightning/src/offers/flow.rs
+++ lightning/src/offers/flow.rs
@@ -388,6 +388,7 @@ where
 		Ok(())
 	}
 
+	#[cfg(feature = "dnssec")]
 	fn enqueue_dns_onion_message(
 		&self, message: DNSSECQuery, dns_resolvers: Vec<Destination>,
 		reply_paths: Vec<BlindedMessagePath>,
