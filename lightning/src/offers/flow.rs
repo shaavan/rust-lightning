1e2012dd6ed4e8975fde668c6553768841f726be
shaavan <shaavan.github@gmail.com>
2025-03-17 15:57:18 +0530

    Introduce OffersMessageFlow

--- lightning/src/offers/flow.rs
+++ lightning/src/offers/flow.rs
@@ -11,16 +11,21 @@
 //! facilitating communication, and handling Bolt12 messages and payments.
 
 use core::ops::Deref;
+use core::sync::atomic::{AtomicUsize, Ordering};
 use core::time::Duration;
 
-use bitcoin::secp256k1;
+use bitcoin::block::Header;
+use bitcoin::constants::ChainHash;
+use bitcoin::secp256k1::{self, PublicKey, Secp256k1};
 
 use crate::blinded_path::message::{
 	AsyncPaymentsContext, MessageContext, MessageForwardNode, OffersContext,
 };
-use crate::chain;
+use crate::chain::transaction::TransactionData;
+use crate::chain::{self, BestBlock};
 use crate::ln::channel_state::ChannelDetails;
-use crate::ln::channelmanager::PaymentId;
+use crate::ln::channelmanager::{ChainParameters, PaymentId};
+use crate::ln::inbound_payment;
 use crate::offers::invoice::{Bolt12Invoice, DerivedSigningPubkey, InvoiceBuilder};
 use crate::offers::invoice_request::{
 	InvoiceRequest, InvoiceRequestBuilder, VerifiedInvoiceRequest,
@@ -31,9 +36,12 @@ use crate::offers::parse::Bolt12SemanticError;
 use crate::offers::refund::{Refund, RefundBuilder};
 use crate::onion_message::async_payments::AsyncPaymentsMessage;
 use crate::onion_message::dns_resolution::HumanReadableName;
-use crate::onion_message::messenger::{Destination, MessageSendInstructions};
+use crate::onion_message::messenger::{Destination, MessageRouter, MessageSendInstructions};
 use crate::onion_message::offers::OffersMessage;
+use crate::routing::router::Router;
+use crate::sign::EntropySource;
 use crate::sign::NodeSigner;
+use crate::sync::{Mutex, RwLock};
 use crate::types::payment::{PaymentHash, PaymentSecret};
 
 #[cfg(async_payments)]
@@ -117,7 +125,6 @@ pub trait Flow: chain::Listen {
 		&self,
 	) -> Vec<(OffersMessage, MessageSendInstructions)>;
 
-	#[cfg(async_payments)]
 	fn get_and_clear_pending_async_messages(
 		&self,
 	) -> Vec<(AsyncPaymentsMessage, MessageSendInstructions)>;
@@ -126,4 +133,130 @@ pub trait Flow: chain::Listen {
 	fn get_and_clear_pending_dns_messages(
 		&self,
 	) -> Vec<(DNSResolverMessage, MessageSendInstructions)>;
+}
+
+pub struct OffersMessageFlow<ES: Deref, MR: Deref, R: Deref>
+where
+	ES::Target: EntropySource,
+	MR::Target: MessageRouter,
+	R::Target: Router,
+{
+	chain_hash: ChainHash,
+	best_block: RwLock<BestBlock>,
+
+	our_network_pubkey: PublicKey,
+	highest_seen_timestamp: AtomicUsize,
+	inbound_payment_key: inbound_payment::ExpandedKey,
+
+	secp_ctx: Secp256k1<secp256k1::All>,
+	entropy_source: ES,
+
+	message_router: MR,
+	router: R,
+
+	#[cfg(not(any(test, feature = "_test_utils")))]
+	pending_offers_messages: Mutex<Vec<(OffersMessage, MessageSendInstructions)>>,
+	#[cfg(any(test, feature = "_test_utils"))]
+	pub(crate) pending_offers_messages: Mutex<Vec<(OffersMessage, MessageSendInstructions)>>,
+
+	pending_async_payments_messages: Mutex<Vec<(AsyncPaymentsMessage, MessageSendInstructions)>>,
+
+	#[cfg(feature = "dnssec")]
+	pending_dns_onion_messages: Mutex<Vec<(DNSResolverMessage, MessageSendInstructions)>>,
+}
+
+impl<ES: Deref, MR: Deref, R: Deref> OffersMessageFlow<ES, MR, R>
+where
+	ES::Target: EntropySource,
+	MR::Target: MessageRouter,
+	R::Target: Router,
+{
+	/// Creates a new [`OffersMessageFlow`]
+	pub fn new(
+		params: ChainParameters, our_network_pubkey: PublicKey, current_timestamp: u32,
+		inbound_payment_key: inbound_payment::ExpandedKey, entropy_source: ES, message_router: MR,
+		router: R,
+	) -> Self {
+		let mut secp_ctx = Secp256k1::new();
+		secp_ctx.seeded_randomize(&entropy_source.get_secure_random_bytes());
+
+		Self {
+			chain_hash: ChainHash::using_genesis_block(params.network),
+			best_block: RwLock::new(params.best_block),
+
+			our_network_pubkey,
+			highest_seen_timestamp: AtomicUsize::new(current_timestamp as usize),
+			inbound_payment_key,
+
+			secp_ctx,
+			entropy_source,
+
+			message_router,
+			router,
+
+			pending_offers_messages: Mutex::new(Vec::new()),
+			pending_async_payments_messages: Mutex::new(Vec::new()),
+			#[cfg(feature = "dnssec")]
+			pending_dns_onion_messages: Mutex::new(Vec::new()),
+		}
+	}
+
+	/// Gets the node_id held by this OffersMessageFlow
+	pub fn get_our_node_id(&self) -> PublicKey {
+		self.our_network_pubkey
+	}
+
+	fn duration_since_epoch(&self) -> Duration {
+		#[cfg(not(feature = "std"))]
+		let now = Duration::from_secs(self.highest_seen_timestamp.load(Ordering::Acquire) as u64);
+		#[cfg(feature = "std")]
+		let now = std::time::SystemTime::now()
+			.duration_since(std::time::SystemTime::UNIX_EPOCH)
+			.expect("SystemTime::now() should come after SystemTime::UNIX_EPOCH");
+		now
+	}
+
+	fn best_block_updated(&self, header: &Header) {
+		macro_rules! max_time {
+			($timestamp: expr) => {
+				loop {
+					// Update $timestamp to be the max of its current value and the block
+					// timestamp. This should keep us close to the current time without relying on
+					// having an explicit local time source.
+					// Just in case we end up in a race, we loop until we either successfully
+					// update $timestamp or decide we don't need to.
+					let old_serial = $timestamp.load(Ordering::Acquire);
+					if old_serial >= header.time as usize {
+						break;
+					}
+					if $timestamp
+						.compare_exchange(
+							old_serial,
+							header.time as usize,
+							Ordering::AcqRel,
+							Ordering::Relaxed,
+						)
+						.is_ok()
+					{
+						break;
+					}
+				}
+			};
+		}
+
+		max_time!(self.highest_seen_timestamp);
+	}
+}
+
+impl<ES: Deref, MR: Deref, R: Deref> chain::Listen for OffersMessageFlow<ES, MR, R>
+where
+	ES::Target: EntropySource,
+	MR::Target: MessageRouter,
+	R::Target: Router,
+{
+	fn filtered_block_connected(&self, header: &Header, _txdata: &TransactionData, _height: u32) {
+		self.best_block_updated(header);
+	}
+
+	fn block_disconnected(&self, _header: &Header, _height: u32) {}
 }
