3d6f0683c9d0b54e8c8923ee6dd2b5ccad00b6bf
shaavan <shaavan.github@gmail.com>
2025-04-16 18:46:44 +0530

    Introduce Flow parameter in ChannelManager

--- lightning/src/offers/flow.rs
+++ lightning/src/offers/flow.rs
@@ -162,7 +162,7 @@ where
 		now
 	}
 
-	fn best_block_updated(&self, header: &Header) {
+	pub(crate) fn best_block_updated(&self, header: &Header) {
 		macro_rules! max_time {
 			($timestamp: expr) => {
 				loop {
