0f2a1ae523a4d65d7ae5c6b096449fc0e901438b
shaavan <shaavan.github@gmail.com>
2025-08-30 19:09:22 +0530

    Introduce recurrence

--- lightning/src/offers/invoice_request.rs
+++ lightning/src/offers/invoice_request.rs
@@ -1550,6 +1550,11 @@ mod tests {
 					issuer: None,
 					quantity_max: None,
 					issuer_id: Some(&recipient_pubkey()),
+					recurrence_compulsory: None,
+					recurrence_optional: None,
+					recurrence_base: None,
+					recurrence_paywindow: None,
+					recurrence_limit: None,
 				},
 				InvoiceRequestTlvStreamRef {
 					chain: None,
