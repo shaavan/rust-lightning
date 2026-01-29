156cabfeccaef12468d3dd9425be35a1992ca112
shaavan <shaavan.github@gmail.com>
2026-01-29 20:10:05 +0530

    Fix tests

--- lightning/src/ln/onion_payment.rs
+++ lightning/src/ln/onion_payment.rs
@@ -683,6 +683,7 @@ where
 			) {
 				Ok((amt, cltv)) => (amt, cltv),
 				Err(()) => {
+					assert!(false);
 					return encode_relay_error("Underflow calculating outbound amount or cltv value for dummy hop",
 						LocalHTLCFailureReason::InvalidOnionBlinding, shared_secret.secret_bytes(), None, &[0; 32]);
 				}
